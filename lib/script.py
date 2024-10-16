c3b070273e3d493e3b63489627bb328c942ce5c7
PESolut <jahaadpetty@pursuit.org>
2024-10-16 15:04:43 -0400

    a user can input a message, with no functionality to track guesses or see if the guess is correct

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,42 @@
 from capitals import states
 
-print(states)
+
+# init var to track guessed capitals
+guessedCapitals = []
+
+print('You need to guess all the capitals my guyza')
+
+# flow of the app
+# 1) check guessedCapitals to see if theres a need for a repsonse
+    # if not; tell the user he won
+# 2) prompt user to guess
+# 3) check if theres any match between guess to each state within our DS
+    # if so, remove that state from our list, add that capital to guessed capitals and keep the loop going
+    # if not, tell the user hes wrong and restart the loop
+
+#guard clause, check if guessedCapitals is full...
+while 1:
+    guessedCapitalsLen = len(guessedCapitals)
+
+    if guessedCapitalsLen == 50:
+    # tell the user he won.
+        print('you won')
+    # otherwise move on
+
+    print('you guessed '+ str(guessedCapitalsLen) + ' capitals correct so far')
+    guess = input("Please enter your Guess:")
+
+    # we need to mutate the states dictionary so it does not include any object to which the capital key == currentElCap
+    
+    # for capital in states:
+    #     currentElCap = (capital)
+    #     print(currentElCap)
+    #     if currentElCap in states:
+    #         print('FLAG')
+            # states = {key: val for key,
+            #           val in states.items() if key != 'currentElCap'}
+            
+    # print(states)
+
+
+
